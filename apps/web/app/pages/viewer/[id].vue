aac612022c111ba1e8d220b0172679f87692482a
Timur <toombez.timurtokaev@gmail.com>
2025-09-25 10:43:31 +0300

    Add viewer page

---
+++ apps/web/app/pages/viewer/[id].vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+const { id: index } = useRoute().params
+
+const { data: slides } = useFetch('/api/get-slides-links')
+
+const targetSlides = computed(() => {
+    if (typeof index === 'undefined') {
+        return
+    }
+
+    if (typeof slides.value === 'undefined') {
+        return
+    }
+
+    return slides.value?.[+index]
+})
+</script>
+
+<template>
+    <UPageBody>
+        <div v-if="targetSlides">
+            <UContainer>
+                <h1 class="text-5xl font-bold mb-4">
+                    {{ targetSlides.title }}
+                </h1>
+                <ULink :to="targetSlides.link" external target="_blank">
+                    <span>
+                        Открыть в новом окне
+                    </span>
+
+                    <UIcon name="lucide:arrow-up-right" />
+                </ULink>
+
+                <iframe
+                    :src="targetSlides.link"
+                    class="aspect-video w-full my-6"
+                />
+
+                <h2 class="text-4xl font-bold mb-4">
+                    Теоретический материал
+                </h2>
+
+                <ol>
+                    <li
+                        v-for="theory in targetSlides.theory"
+                    >
+                        <ULink :to="theory.link">
+                            <span>
+                                {{ theory.title }}
+                            </span>
+
+                            <UIcon name="lucide:arrow-up-right" />
+                        </ULink>
+                    </li>
+                </ol>
+            </UContainer>
+        </div>
+    </UPageBody>
+</template>
