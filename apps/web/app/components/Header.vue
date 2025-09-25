d8af7cfd004f60468a90b5dbdf2abed158ddf9be
Timur <toombez.timurtokaev@gmail.com>
2025-09-25 10:41:08 +0300

    Add Header

---
+++ apps/web/app/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+    <UContainer>
+        <UPageHeader>
+            <template #title>
+                <ULink to="/">
+                    Лекции и слайды "Системное программирование"
+                </ULink>
+            </template>
+        </UPageHeader>
+    </UContainer>
+</template>
